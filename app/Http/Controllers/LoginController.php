5419072550ac384178de119b97de02f1be92d736
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-03-19 16:29:56 -0300

    LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Socialite;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+
+    use AuthenticatesUsers;
+    protected $redirectTo = '/';
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('senhaunica')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $userSenhaUnica = Socialite::driver('senhaunica')->user();
+        $user = User::where('codpes',$userSenhaUnica->codpes)->first();
+
+        if (is_null($user)) $user = new User;
+
+        // bind do dados retornados
+        $user->codpes = $userSenhaUnica->codpes;
+        $user->email = $userSenhaUnica->email;
+        $user->name = $userSenhaUnica->nompes;
+        $user->save();
+        Auth::login($user, true);
+        return redirect('/');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',[LoginController::class, 'redirectToProvider'])->name('login');
+Route::get('callback', [LoginController::class, 'handleProviderCallback']);
+Route::get('logout',[LoginController::class, 'logout'])->name('logout');
