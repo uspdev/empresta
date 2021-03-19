a1ff910969842a1da66133c3bfe040fb95d66eb7
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-03-19 16:04:01 -0300

    WIP - CRUD

---
+++ app/Http/Controllers/EmprestimoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Emprestimo;
+use Illuminate\Http\Request;
+
+class EmprestimoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Emprestimo  $emprestimo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Emprestimo $emprestimo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Emprestimo  $emprestimo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Emprestimo $emprestimo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Emprestimo  $emprestimo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Emprestimo $emprestimo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Emprestimo  $emprestimo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Emprestimo $emprestimo)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/VisitanteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitante;
+use Illuminate\Http\Request;
+
+class VisitanteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Visitante  $visitante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visitante $visitante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Visitante  $visitante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Visitante $visitante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Visitante  $visitante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Visitante $visitante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Visitante  $visitante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visitante $visitante)
+    {
+        //
+    }
+}
