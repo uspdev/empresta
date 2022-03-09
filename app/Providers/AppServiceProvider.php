d7a170fe79f669bb072fafe49699afc286bdfb34
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-03-08 21:32:13 -0300

    https support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+        // força https na produção
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
