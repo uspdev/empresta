6398e7590ec0fafa8db01080e9e532727cb4df69
Masaki Kawabata Neto <kawabata@usp.br>
2022-05-13 15:35:35 -0300

    configurando para versão nova

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -123,13 +123,16 @@ $menu = [
 
 $right_menu = [
     [
-        'text' => '<i class="fas fa-cog"></i>',
-        'title' => 'Configurações',
-        'target' => '_blank',
-        'url' => 'item1',
-        'align' => 'right',
-        'can' => 'admin',
+        'key' => 'senhaunica-socialite',
     ],
+    // [
+    //     'text' => '<i class="fas fa-cog"></i>',
+    //     'title' => 'Configurações',
+    //     'target' => '_blank',
+    //     'url' => 'item1',
+    //     'align' => 'right',
+    //     'can' => 'admin',
+    // ],
     [
         'text' => '<i class="fas fa-hard-hat"></i>',
         'title' => 'Logs',
@@ -140,16 +143,27 @@ $right_menu = [
     ],
 ];
 
-# dashboard_url renomeado para app_url
-# USPTHEME_SKIN deve ser colocado no .env da aplicação 
-
 return [
+    # valor default para a tag title, dentro da section title.
+    # valor pode ser substituido pela aplicação.
     'title' => config('app.name'),
+
+    # USP_THEME_SKIN deve ser colocado no .env da aplicação 
     'skin' => env('USP_THEME_SKIN', 'uspdev'),
+
+    # chave da sessão. Troque em caso de colisão com outra variável de sessão.
+    'session_key' => 'laravel-usp-theme',
+
+    # usado na tag base, permite usar caminhos relativos nos menus e demais elementos html
+    # na versão 1 era dashboard_url
     'app_url' => config('app.url'),
+
+    # login e logout
     'logout_method' => 'POST',
     'logout_url' => 'logout',
     'login_url' => 'login_intern',
+
+    # menus
     'menu' => $menu,
     'right_menu' => $right_menu,
 ];
