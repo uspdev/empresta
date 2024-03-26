423c29029929d09320a4253bac167170029f5f16
Kawan Santana <kawansantana18@gmail.com>
2024-03-26 14:37:07 -0300

    Criando tabela 'vinculos'

---
+++ database/migrations/2024_03_26_142826_create_vinculos_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vinculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('permission');
+            $table->timestamps();
+        });
+
+
+        $permissoesVinculo = [
+            'Aluno Convênoi Interc Grad' => 'Alunoconvenioint',
+            'Aluno de Cultura e Extensão' => 'Alunoceu',
+            'Aluno de Graduação' => 'Alunogr',
+            'Aluno de Pós-Graduação' => 'Alunopos',
+            'Aluno Escola de Arte Dramática' => 'Alunoead',
+            'Docente' => 'Docente',
+            'Docente Aposentado' => 'Servidor',
+            'Estagiário' => 'Estagiario',
+            'Pós-doutorando' => 'Alunopd',
+            'Servidor' => 'Servidor',
+        ];
+
+        foreach($permissoesVinculo as $nome => $permission){
+            \App\Models\Vinculo::create([
+                'nome' => $nome,
+                'permission' => $permission
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vinculos');
+    }
+};
