74c73d267ae5ff159934477933f19b0261017074
Kawan Santana <kawansantana18@gmail.com>
2024-04-08 09:30:05 -0300

    Adicionando devolucao e prazo na tabela materials

---
+++ database/migrations/2024_04_08_092204_add_devolucao_and_prazo_columns_to_materials_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('materials', function (Blueprint $table) {
+            $table->tinyInteger('devolucao')->default(0);
+            $table->integer('prazo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('materials', function (Blueprint $table) {
+            $table->dropColumn('devolucao');
+            $table->dropColumn('prazo');
+        });
+    }
+};
